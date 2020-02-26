53390dd8b9df65717adecd68d0900d26dc1fe01d
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-26 08:19:03 +0800

    Conneting this app to Eureka naming server

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,14 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 // Enable Feign REST client to scan the client, and copy the package name above
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice")
+// Allow this app to be recognized by Eureka naming server
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
