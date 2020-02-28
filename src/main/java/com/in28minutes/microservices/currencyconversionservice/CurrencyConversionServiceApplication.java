9ac6f105aaec3a9fca06bb730850a93858ca1e4b
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-28 22:14:30 +0800

    Added Distributed Tracing with Spring Cloud Sleuth by Zipkin

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 // Enable Feign REST client to scan the client, and copy the package name above
@@ -16,4 +18,9 @@ public class CurrencyConversionServiceApplication {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
 
+	// Trace all requests
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
